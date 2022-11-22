f43a1ba1b5d4c4ddd12fced2e9bfafd65e49e294
Georgina Ruiz <ginru2003@gmail.com>
2022-11-22 08:27:40 -0600

    holaa

--- src/main/java/com/example/practicapia1/controller/holaController.java
+++ src/main/java/com/example/practicapia1/controller/holaController.java
@@ -6,12 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
 public class holaController {
-    @RequestMapping
-    @ResponseBody
-    public ResponseEntity<String> holaMundo() {
-        return new ResponseEntity("Hola mundo", HttpStatus.ACCEPTED);
-    }
+
 
 }
